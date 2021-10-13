2a38a0e895ce17c1214ee4b9dfdbc3fd2ee8118b
sakawatchy434 <hossainshakawat434@gmail.com>
2021-10-13 23:07:58 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-b-1XoY-6JmPIVxxTpLtJQ1F5W6nrZhk",
+    authDomain: "ema-john-simple-adb4c.firebaseapp.com",
+    projectId: "ema-john-simple-adb4c",
+    storageBucket: "ema-john-simple-adb4c.appspot.com",
+    messagingSenderId: "122463519974",
+    appId: "1:122463519974:web:7df40d38e95e7a5124e727"
+};
+
+
+export default firebaseConfig;
---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./Firebase.config";
+
+
+const initAuthentication = () => {
+    initializeApp(firebaseConfig);
+}
+
+
+
+export default initAuthentication;
